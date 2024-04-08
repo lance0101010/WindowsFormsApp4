7bd3a8d2da1d1c5aca3cc2f0eb7ef3a19942ef4d
lance0101010 <lancekkrcop@gmail.com>
2024-04-08 15:25:03 +0800

    2024/04/08
    1.加入計算機的介面
    2.加入數字按鈕的程式
    3.數字鍵-0不會在沒有其他數字時增加
    如:00000......

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,64 @@ namespace WindowsFormsApp4
         {
             InitializeComponent();
         }
+        private void textBoxTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBoxTotal.Text = textBoxTotal.Text + "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (textBoxTotal.Text == "0")
+            {
+                textBoxTotal.Text = "";
+            }
+            textBoxTotal.Text = textBoxTotal.Text + "0";
+        }
     }
 }
